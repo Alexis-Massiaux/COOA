25fea804bc64f3e9e3dcf0b6cfcc4d81b9c1bddf
Alexis Massiaux <alexis.massiaux@univ-lille1.fr>
2016-12-15 14:15:08 +0100

    DecorateurIngredient

--- TP4/src/DecorateurIngredient.java
+++ TP4/src/DecorateurIngredient.java
@@ -1,4 +1,14 @@
 
 public abstract class DecorateurIngredient {
+	
+	protected Dessert dessert; 
+	
+	public String getLibelle() {
+		return dessert.getLibelle();
+	}
+	
+	public double getPrix(){
+		return dessert.getPrix();
+	}
 
 }
